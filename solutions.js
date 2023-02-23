5c7fb4389ae64889419d93c65559f987f98cfab3
Asia Autrey <asia.autrey0@gmail.com>
2023-02-23 09:05:36 -0600

    add assesment

--- solutions.js
+++ solutions.js
@@ -21,3 +21,72 @@
  * concat
  */
 
+//Define a function named isTrue that takes in any input and returns true if the input provided is exactly equal to true in value and data type
+
+
+function isTrue(number){
+    return number===true;
+}
+
+//Define a function named isFalse that takes in a value and returns a true if and only if the provided input is equal to false in both type and value.
+
+function isFalse(value1) {
+ return value1 ===false
+}
+
+//Define a function named not that takes in any input and returns the boolean opposite of the provided input.
+
+function not(value) {
+   return(!value)
+}
+
+
+
+//Define a function named addOne that takes in a single input.
+// If the input is a number or a numeric string, return the value plus one.
+
+ function addOne(input){
+          if(typeof input === "number")
+              return input + 1
+          else if(typeof input == 'string' )
+              return (+input) + 1
+
+}
+//Define a function named isEven that takes in a single input. If the input is an even number or a string containing an even number, return true. Any other input should return false for the output.
+
+
+    function isEven(input){
+     return input % 2 === 0
+
+}
+
+//Define a function named isEqual that takes in two input arguments. If each argument is equal only in value, then return true. Otherwise return false.
+  function isEqual(input1,input2) {
+      return input1 == input2;
+      // return true
+      //else if(input1 != input2)
+      //return false
+  }
+
+//Define a function named isIdentical that takes in two input arguments. If each input is equal both in data type and in value, then return true. If the values are not the same data type or not the same value, return false.
+function isIdentical(input1,input2){
+    return input1===input2
+}
+//Define a function named or that takes in two input arguments. The output returned should be the result of an or operation on both inputs.
+
+
+function or(input1,input2){
+    return input1||input2
+}
+
+//Define a function named and that takes in two input arguments and returns the result of a logical and operation of both inputs.
+function and(input1,input2){
+    return input1 && input2
+}
+//Define a function named concat that takes in two input arguments. If both arguments are strings, then return the concatenated result. If two numbers are provided, then return the string concatenation of each set of numerals.
+
+function concat(input1,input2){
+    return input1 +''+ input2+''
+}
+
+
